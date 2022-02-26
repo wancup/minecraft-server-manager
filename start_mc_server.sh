5e1e1b5d9dccfce63db4b07c4cc99e0b7f589513
wancup <62416191+wancup@users.noreply.github.com>
2022-02-26 19:45:22 +0900

    Fix 起動スクリプト

--- start_mc_server.sh
+++ start_mc_server.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
+minecraft_path=["your-minecraft-path"]
+
 # backup
 # If you want to upload world data to s3, uncomment next block
 # s3_bucket_name=["your-backup-bucket-name"]
-# backup_file_name=/home/ec2-user/backup/mc_world_$(date "+%Y%m%d").tar.gz
-# tar -zcvf "$backup_file_name" /home/ec2-user/world
+# backup_file_name="$minecraft_path"/backup/mc_world_$(date "+%Y%m%d").tar.gz
+# tar -zcvf "$backup_file_name" "$minecraft_path"/world
 # aws s3 cp "$backup_file_name" s3://"$s3_bucket_name"/"$(date '+%Y%m')"/
 # rm -f "$backup_file_name"
 
 # start sever
-java -Xms1024M -Xmx2048M -jar /home/ec2-user/minecraft_server.xxx.jar nogui
+java -Xms1024M -Xmx2048M -jar "$minecraft_path"/minecraft_server.xxx.jar nogui
 
